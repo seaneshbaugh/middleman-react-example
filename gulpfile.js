a5db839328dbedc85b79071f814ac30ad8d66c48
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-03-09 15:18:20 -0600

    Make use of Gulps file watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
-var babel, concat, gulp;
+var gulp, babel, concat, sourcemaps, jsFiles;
 
+gulp = require("gulp");
 babel = require("gulp-babel");
 concat = require("gulp-concat");
-gulp = require("gulp");
+sourcemaps = require("gulp-sourcemaps");
 
-gulp.task("default", function() {
-    return gulp.src([
+jsFiles = {
+    vendor: [
         "./node_modules/react/dist/react.js",
-        "./node_modules/react-dom/dist/react-dom.js",
-        "./source/javascripts/**/*"])
-        .pipe(babel())
+        "./node_modules/react-dom/dist/react-dom.js"
+    ],
+    source: [
+        "./source/javascripts/**/*"
+    ]
+}
+
+gulp.task("concat", function() {
+    return gulp.src(jsFiles.vendor.concat(jsFiles.source))
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            compact: false
+        }))
         .pipe(concat("all.js"))
+        .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest("./dist/javascripts/"));
 });
+
+gulp.task("watch", function() {
+    gulp.watch("./source/javascripts/**/*", ["concat"]);
+});
+
+gulp.task("default", ["concat", "watch"]);
